06c588f0a84e48795c548bf293e3a56075271277
Petar Marjanovic <petarmarjanovic12@yahoo.com>
2018-08-12 18:42:09 +0200

    fixed path

--- scripts/select-fighter.js
+++ scripts/select-fighter.js
@@ -2,12 +2,12 @@ const mute = document.querySelector('.mute');
 const button = document.querySelectorAll('.character');
 const select = document.querySelector('.select');
 
-var audio = new Audio('../audio/char-select-sound.mp3');
+var audio = new Audio('./audio/char-select-sound.mp3');
 audio.loop = true;
 
 button.forEach(btn => {
     btn.addEventListener('mouseenter', () => {
-        var audio = new Audio('../audio/main-menu-hover.mp3');
+        var audio = new Audio('./audio/main-menu-hover.mp3');
         audio.play();
     })
 })
@@ -25,12 +25,12 @@ mute.addEventListener('click', () => {
 })
 
 function goToFight() {
-    var clickAudio = new Audio('../audio/go-to-fight.mp3');
+    var clickAudio = new Audio('./audio/go-to-fight.mp3');
     clickAudio.play();
     audio.volume = 0.3;
     
     setTimeout(() => {
-        window.location.href = "../fight.html";
+        window.location.href = "./fight.html";
     }, 3000);
 }
 
